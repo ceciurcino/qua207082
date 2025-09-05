8108f64138165a160478176d0dfc7aec2890fe3f
Cecilia <ceciliaurcino@gmail.com>
2025-09-04 21:03:09 -0300

    correção resp4_c

--- livrojs/cap04/js/resp4_c.js
+++ livrojs/cap04/js/resp4_c.js
@@ -5,25 +5,23 @@ const resp2 = document.querySelector('#outResp2')
 frm.addEventListener('submit', (e) => {
     e.preventDefault()
     const valor = Number(frm.inValor.value)
-    let tempo
     let troco
     let valorTotal
-    if(valor < 1){
+    if (valor < 1) {
         resp1.innerText = `Valor Insuficiente`
-    } else if(valor >= 1 && valor < 1.75){
-        tempo = 30
+    } else if (valor >= 1 && valor < 1.75) {
         troco = valor - 1.00
-        resp1.innerText = `Tempo: ${tempo}min`
+        resp1.innerText = `Tempo: 30min`
         resp2.innerText = `Troco: ${troco.toFixed(2)} `
     } else if (valor >= 1.75 && valor < 3.00) {
         tempo = 60
         troco = valor - 1.75
-        resp1.innerText = `Tempo: ${tempo}min`
+        resp1.innerText = `Tempo: 60min`
         resp2.innerText = `Troco: ${troco.toFixed(2)}`
-    } else if (valor >=3) {
+    } else if (valor >= 3) {
         tempo = 120
         troco = valor - 3.00
-        resp1.innerText = `Tempo: ${tempo}`
+        resp1.innerText = `Tempo: 120min`
         resp2.innerText = `Troco: ${troco.toFixed(2)}`
     }
 })
