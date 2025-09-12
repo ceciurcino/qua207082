71eeffa2cf3063183cad48a33edc31a06fb69a27
Cecilia <ceciliaurcino@gmail.com>
2025-09-12 20:06:49 -0300

    livrojs: resp5_c

--- livrojs/cap05/js/resp5_c.js
+++ livrojs/cap05/js/resp5_c.js
@@ -5,6 +5,21 @@ const resp2 = document.querySelector('#outResp2')
 frm.addEventListener('submit', (e) => {
     e.preventDefault()
     const numero = Number(frm.inNumero.value)
-    let soma = 0
     let divisores = ""
+    let soma = 0
+
+    for(let i = 1; i < numero; i++){
+        if(numero % i === 0){
+         soma += i
+         divisores += i + ","
+        }
+    }
+
+    resp1.innerText = `Divisores do ${numero}: ${divisores} (Soma: ${soma})`
+
+    if(soma === numero){
+        resp2.innerText = `${numero} É um Número Perfeito`
+    } else {
+        resp2.innerText = `${numero} não é um Número Perfeito`
+    }
 })
