ad72833cb2084ba7c5d2685f389b12012f330686
Cecilia <ceciliaurcino@gmail.com>
2025-09-12 20:06:32 -0300

    correção resp5_b

--- livrojs/cap05/js/resp5_b.js
+++ livrojs/cap05/js/resp5_b.js
@@ -3,17 +3,12 @@ const resp = document.querySelector('pre')
 
 frm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const numero = Number(frm.inNumero.value)
+    let numero = Number(frm.inNumero.value)
     const anos = Number(frm.inAnos.value)
-    let resposta = ""
-    let total = numero
 
     for (let i = 1; i <= anos; i++) {
-        resposta += i + "º Ano:" + total + "\n"
-
-        if (i >= 1) { // só multiplica a partir do 2º ano
-            total *= 3 // triplica a cada ano
+          resp.innerText += `${i}º Ano: ${numero} Chinchilas\n`
+            numero *= 3 // triplica a cada ano
         }
-    }
-    resp.innerText = resposta
+    
 })
