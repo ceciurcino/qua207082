4ee21c5d000f1d90ee7daa54dc483f9ac02b8e1a
Cecilia <ceciliaurcino@gmail.com>
2025-09-18 21:45:34 -0300

    livrojs: começo do Exercício 6.a

---
+++ livrojs/cap06/js/resp6_a.js
@@ -0,0 +1,11 @@
+const frm = document.querySelector('form')
+const resp = documento.querySelector('pre')
+const clubes = [] // vetor global para armazenar os clubes
+// incluir clubes fixos
+clubes.push("Flamengo", "Palmeiras", "Santos", "Corinthians")
+
+// adicionar clube
+
+
+
+
