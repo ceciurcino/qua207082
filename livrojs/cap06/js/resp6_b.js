dcf8cac6b6ad89aef57b0498c5ce5f5cbd4855e5
Cecilia <ceciliaurcino@gmail.com>
2025-09-22 21:47:49 -0300

    correção resp6_b

--- livrojs/cap06/js/resp6_b.js
+++ livrojs/cap06/js/resp6_b.js
@@ -1,51 +1,35 @@
 const frm = document.querySelector('form')
-const resp = document.querySelector('pre')
+const resp1 = document.querySelector('#outResp1')
+const resp2 = document.querySelector('#outResp2')
+
 const numeros = []
 
-// Adiciona um Event Listener no formulário para o evento 'submit'
+
 frm.addEventListener('submit', (e) => {
     e.preventDefault()
     const numero = Number(frm.inNumero.value)
-    
     // Verifica se o número já existe no vetor
-    if (numeros.includes(numero)) {
-        alert('Este número já foi incluído na lista.');
-        frm.inNumero.value = '';
-        frm.inNumero.focus();
-        return;
+    if(numeros.includes(numero) == true){
+        alert('Número repetido')
+        return
     }
-
-    numeros.push(numero);
+    numeros.push(numero)
+    resp1.innerText = 'Numeros: '+numeros.join(', ')
     frm.inNumero.value = '';
     frm.inNumero.focus();
-    
-    if (numeros.length === 0) {
-        resp.innerText = 'Nenhum número foi adicionado.';
-    } else {
-        const lista = numeros.join(', ');
-        resp.innerText = `Números: ${lista}`;
-    }
 })
 
-// Adiciona um Event Listener ao botão "Verificar Ordem"
 frm.btVerificar.addEventListener('click', () => {
-    if (numeros.length === 0) {
-        resp.innerText = 'Nenhum número foi adicionado.';
-        return;
-    }
-    
-    let estaEmOrdem = true;
-
-    for (let i = 1; i < numeros.length; i++) {
-        if (numeros[i] < numeros[i - 1]) {
-            estaEmOrdem = false;
-            break;
+    let ordem = true
+    for(let i = 1; i < numeros.length; i++){
+        if(numeros[i - 1] > numeros[i]){
+            ordem = false
+            break
         }
     }
-
-    if (estaEmOrdem) {
-        resp.innerText = 'Os números estão em ordem crescente.';
-    } else {
-        resp.innerText = 'Os números NÃO estão em ordem crescente.';
+    if(ordem){
+        resp2.innerText = 'Os números estão em ordem crescente'
+    }else{
+        resp2.innerText = 'Os números NÃO estão em ordem crescente'
     }
-});
+})
