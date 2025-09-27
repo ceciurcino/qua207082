df06dcae0407f4ca51cba374c76094ededb6784a
Cecilia <ceciliaurcino@gmail.com>
2025-09-26 21:06:39 -0300

    resp7_a

--- livrojs/cap07/js/resp7_a.js
+++ livrojs/cap07/js/resp7_a.js
@@ -23,5 +23,34 @@ frm.btCripto.addEventListener("click", () => {
 })
 
 frm.btDecripto.addEventListener("click", () =>{
+    const criptografia = resp.innerText.trim()
+    const tam = criptografia.length
+
+    if(!criptografia){
+        resp.innerText = "Não há uma mensagem para decriptografar."
+        return
+    }
+
+    // Determina os comprimentos das partes par e impar
+    const tamPares = Math.ceil(tam/2)
     
+    // Divide a string criptografia nas duas partes
+    const pares = criptografia.substring(0, tamPares)
+    const impares = criptografia.substring(tamPares)
+
+    // Cria um array vazio
+    let msgDecripto = []
+
+    for(let i = 0; i < tamPares; i++) {
+        // Adiciona sempre o caractere par (que estava no índice i de 'pares')
+        msgDecripto.push(pares[i])
+        // Se ainda houver um caractere ímpar correspondente, adiciona
+        if (impares[i]) {
+        msgDecripto.push(impares[i])
+        }
+    }
+
+    // exibe a mensagem
+    resp.innerText = msgDecripto.join("")
+
 })
