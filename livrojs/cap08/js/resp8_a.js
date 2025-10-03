e930424a53cc7f1db90000be356ad203cbe7a1c4
Cecilia <ceciliaurcino@gmail.com>
2025-10-03 19:55:13 -0300

    livrojs resp8_a

--- livrojs/cap08/js/resp8_a.js
+++ livrojs/cap08/js/resp8_a.js
@@ -1,15 +1,38 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("pre")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nome = frm.inNome.value
+    const idade = Number(frm.inIdade.value)
+    const tracos = retornarTracos(nome)
+    const categoria = categorizarAluno(idade)
+    resp.innerText = nome + "\n" + tracos + "\n" + categoria
+
+})
 // função para adicionar espaços ou - abaixo do nome
 function retornarTracos(nome) {
     let linhaTracos = "" // variável para armazenar o resultado final
-    for(let i = 0; i <= nome.length; i++) {
-        let caractere = nome.[i]
+    for (let i = 0; i < nome.length; i++) {
+        let caractere = nome[i]
+
+        // se o caractere for espaço vazio, adiciona espaço vazio
+        if (caractere == " ") {
+            linhaTracos += " "
+            // se não, adiciona "-"
+        } else {
+            linhaTracos += "-"
+        }
     }
-    // se o caractere for
-    if(caractere == " "){
-        linhaTracos += " "
-    }else {
-        linhaTracos += "-"
+    return linhaTracos
+}
+// função para selecionar a categoria do aluno
+function categorizarAluno(idade) {
+    if (idade >= 0 && idade <= 12) {
+        return "Infantil"
+    } else if (idade <= 18) {
+        return "Juvenil"
+    } else {
+        return "Adulto"
     }
 }
