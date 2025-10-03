6288c62fad4fc87ee8f7e16a4353432e11948c2d
Cecilia <ceciliaurcino@gmail.com>
2025-10-02 21:24:01 -0300

    exemplo calculadora

--- exemplos/calculadora/js/script.js
+++ exemplos/calculadora/js/script.js
@@ -23,5 +23,33 @@ function calcular() {
     } catch (error) {
         visor.value = "Erro"
     }
+}
 
+function porcentagem() {
+    calcular()
+    const visor = document.querySelector("#visor")
+    visor.value = Number(visor.value)/100
+}
+
+const botoes = document.querySelectorAll("button")
+for (const botao of botoes) {
+    botao.addEventListener("click", () => {
+        switch (botao.innerText) {
+            case "C":
+                limpar()
+                break;
+            case "←":
+                apagar()
+                break;
+            case "%":
+                porcentagem()
+                break;
+            case "=":
+                calcular()
+                break;     
+            default:
+                adicionar(botao.innerText)
+                break;
+        }
+    }) 
 }
