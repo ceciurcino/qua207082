a3beefcf0bc83d72e1b39e2cb9adba599c570a5b
Cecilia <ceciliaurcino@gmail.com>
2025-10-03 21:49:14 -0300

    inicio resp8_c

---
+++ livrojs/cap08/js/resp8_c.js
@@ -0,0 +1,46 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+const divConvenio = document.querySelector("#divConvenio")
+
+// quando o radio button sim é clicado
+frm.rbSim.addEventListener("click", () => {
+    divConvenio.className = "exibe"
+})
+
+// quando o radio button nao é clicado
+frm.rbNao.addEventListener("click", () => {
+    // Retorna para a classe "oculta"
+    divConvenio.className = "oculta"; 
+})
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const valor = Number(frm.inValor.value)
+    const convenio = frm.inConvenio.value
+    let taxa = 0
+    if(frm.rbNao.checked){ 
+        taxa = 0.10 // 10% de desconto
+    }
+    
+    if(frm.rbSim.checked){
+        // verifica qual convenio está marcado
+        if(convenio == Amigo) {
+            taxa = 0.20 // 20% de desconto
+        } else {
+            taxa = 0.50 // 50% de desconto
+        }
+    }
+
+    const valorDesconto = calcularDesconto(valor,taxa)
+    const valorFinal = valor - valorDesconto
+    resp1.innerText = `Desconto R$: ${valorDesconto}`
+    resp2.innerText = `A Pagar R$: ${valorFinal}`
+})
+
+// função para calcular o desconto
+function calcularDesconto(valor,taxa){
+    const desconto =  valor * taxa
+    return desconto.toFixed(2)
+}
