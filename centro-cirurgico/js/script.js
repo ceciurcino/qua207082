ce0d15c3627d8358a49c927e41a9dd0655e59d10
Cecilia <ceciliaurcino@gmail.com>
2025-10-16 21:45:06 -0300

    inicio js

---
+++ centro-cirurgico/js/script.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form")
+const tbody = document.querySelector("tbody")
+const cores = {
+    '1': 'pre-operatorio', // Pré-Operatório
+    '2': 'em-cirurgia',    // Em cirurgia
+    '3': 'em-recuperacao', // Em recuperação
+    '4': 'transferido'     // Transferido
+}
+
