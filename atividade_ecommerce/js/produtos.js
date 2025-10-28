6ef8fb0d6d6e6b8be0e512c72bba31b7824c7009
Cecilia <ceciliaurcino@gmail.com>
2025-10-27 21:48:16 -0300

    início atividade ecommerce

---
+++ atividade_ecommerce/js/produtos.js
@@ -0,0 +1,57 @@
+const lsProduto = [
+    {
+        nome: "Banho de Mar",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Passeio na Floresta",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-02.png",
+        valor: 39
+    },
+    {
+        nome: "Trio Bom dia",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-03.png",
+        valor: 39
+    },
+    {
+        nome: "Meus Cristais",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-04.png",
+        valor: 39
+    },
+    {
+        nome: "Campo de Lavanda",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-05.png",
+        valor: 39
+    },
+    {
+        nome: "Minha Rotina",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-06.png",
+        valor: 39
+    },
+    {
+        nome: "Vida no Campo",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-07.png",
+        valor: 39
+    },
+    {
+        nome: "Meu Momento",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-08.png",
+        valor: 39
+    },
+    {
+        nome: "Flores Vivas",
+        descricao: " vela aromática - 300g",
+        img: "img/vela-09.png",
+        valor: 39
+    },
+  
+]
