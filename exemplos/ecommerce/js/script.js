4f14ff7ac41ec88f668a30476197ba3dafb7b21d
Cecilia <ceciliaurcino@gmail.com>
2025-10-27 21:22:46 -0300

    finalização ecommerce

--- exemplos/ecommerce/js/script.js
+++ exemplos/ecommerce/js/script.js
@@ -92,5 +92,9 @@ frm.addEventListener("submit", (e) => {
     let msg = "Desejo fazer os seguinte pedido!\n"
     msg += pedido
     msg += `Att: ${nome}`
-    alert(msg)
+    if(confirm("Deseja enviar essa mensagem?\n"+msg)) {
+        msg = encodeURI(msg)
+        const link = `https://api.whatsapp.com/send?phone=5561982567849&text=${msg}`
+        window.open(link, '_blank') 
+    }
 })
